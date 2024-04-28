f7b095abd5e44addff0a0db0ea761c7e3c954caf
Raul Macarie <raul@macarie.me>
2024-04-28 11:03:39 +0200

    add MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	srcDir: "source",
-	integrations: [tailwind()],
+	integrations: [tailwind(), mdx()],
 });
