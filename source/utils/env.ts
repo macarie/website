94231d9460cb7df2e04a586a5e9b16d771b482f5
Raul Macarie <raul@macarie.me>
2024-08-07 10:17:47 +0000

    pipes

--- source/utils/env.ts
+++ source/utils/env.ts
@@ -3,7 +3,6 @@ import { z } from "astro:content";
 const schema = z.object({
 	NAME: z.string(),
 	ROLE: z.string(),
-	X_USERNAME: z.string().startsWith("@"),
 });
 
 export const env = schema.parse(import.meta.env);
